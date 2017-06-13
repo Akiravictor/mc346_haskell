9d6573da01c0d8fc18c20ae25661805ab07294a1
vig <viglionigr@gmail.com>
2017-06-13 14:46:52 -0300

    y.y

--- proj.hs
+++ proj.hs
@@ -1,17 +1,26 @@
 pri l = head l
 seg l = head (drop 4 l)
 ter l = read (drop 8 l) :: Float
+trd (_,_,x) = x
 
 getVec [] = []
 getVec (x:xs) = zip3 [pri x] [seg x] [ter x] ++ getVec xs
 
+-- isPath [] _ _ = error "error"
+-- isPath (x:[]) a b
+--   | fst x == a && snd x == b = True
+--   | otherwise = False
+-- isPath 
+
+imprime [] = return ()
+imprime (x:xs) = do
+  print x
+  imprime xs
 main = do
   entrada <- getContents
   let linhas = lines entrada
   let regras = getVec (take (length linhas - 2) linhas)
   let inicial = head (drop (length linhas -2) linhas)
   let final = last linhas
-  print regras
-  print inicial
-  print final
+  imprime  regras
 
